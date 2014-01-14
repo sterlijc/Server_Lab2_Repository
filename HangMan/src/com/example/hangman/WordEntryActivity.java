d37f52b49f5e9617c3ecea04c2aab753cb4422d6
Dan Griffin <dcompgriff@gmail.com>
2014-01-14 17:35:54 -0500

    Implemented WordEntryActivity.
    The WordEntryActivity has been implemented for the project. It has not
    yet been tested.
    
    Change-Id: I58d5abc224b9e6e9cc32af29ca54a0e767487825
    Signed-off-by: Dan Griffin <dcompgriff@gmail.com>

--- HangMan/src/com/example/hangman/WordEntryActivity.java
+++ HangMan/src/com/example/hangman/WordEntryActivity.java
@@ -4,9 +4,13 @@
 */
 package com.example.hangman;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * <h2>Overview</h2>
@@ -18,11 +22,16 @@ import android.view.Menu;
  * 
  * */
 public class WordEntryActivity extends Activity {
-
+	
+	public final static String EXTRA_MESSAGE = "com.example.hangman.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.word_entry_layout);
+		
+		
+		
 	}
 
 	@Override
@@ -30,6 +39,32 @@ public class WordEntryActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void setWord(View view)
+	{
+		Intent intent = new Intent(this, HangmanActivity.class);
+		EditText wordEditText = (EditText) findViewById(R.id.enteredWordEditText);
+		
+		// Check the number of characters in the word
+		String word = wordEditText.getText().toString();
+		if (word.length() > 6)
+		{
+			Toast.makeText(wordEditText.getContext(), "Enter a word less than 6 characters!", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		else if (word.length() == 0)
+		{
+			Toast.makeText(wordEditText.getContext(), "Enter a word!", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		else
+		{
+			intent.putExtra(EXTRA_MESSAGE, word);
+			
+			startActivity(intent);
+		}
+			
 	}
 
 }
